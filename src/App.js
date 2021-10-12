e88f57b5a3e22bee80c9e15152d6c542242456b0
klongrich <longrichk@gmail.com>
2021-10-12 18:25:53 -0400

    adding part meta

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import React, { useState } from "react";
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
 
+//Eiko
+//Part ID : Order ID  : # of Peices
 const PartsList = [
-  'Light One',
-  'Light Two'
+  'LED13WT8/U6/840-G9DM : 10927 : 15',
+  'LED12W4PH/G24Q/840DM-G7 : 10168 : 24',
+  'LLW2-220-20W-840-U : 11092 : 9',
+  'EMB-CP-5WBT-Y : NULL : 9',
+  'LLW-440-40W-840-U: 11094 : 6',
+  'LED12WT8/48/850-G8DM : 10103 : 25',
+  'LED12WT8/48/840-DBL-A : NULL : 31',
+  'DKDS-6-15W/10.5/940-DIM-120 : 10301 : 14',
+  'SSD6-84-12W-9CCT3-120DT-WH : 11287 : 12',
+  'SDL6RBAFCCTDWW : 10817 : 2',
+  'CDR-8-10/85/840-SSW-DIM-U : 10117 : 2',
+  'LED43WT8/96/850-R17D-G8 : 10105 : 14',
+  'LED11WPAR30/FL/840-DIM-G9 : 10768 : 18',
+  'XELEUM-8-FOOT : NULL : 2',
+  'LED8WA19/OMN/830-DIM-B : 11075 : 11'
 ];
 
 // const PartsMeta = [
