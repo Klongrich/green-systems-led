a1c170ccad953ab932afc24e04b5263ac50c51e4
klongrich <longrichk@gmail.com>
2021-10-12 17:32:14 -0400

    finishing touches

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 
-import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
-import Button from "@material-ui/core/Button";
 
 const PartsList = [
   'Light One',
   'Light Two'
 ];
 
-const PartsMeta = [
-  {
-    "name": "Light One",
-    "id": "08-UL-56"
-  },
-  {
-    "name": "Light Two",
-    "id": "07-W2-LLS"
-  }
-]
-
+// const PartsMeta = [
+//   {
+//     "name": "Light One",
+//     "id": "08-UL-56",
+//     "location": "A1"
+//   },
+//   {
+//     "name": "Light Two",
+//     "id": "07-W2-LLS",
+//     "location": "A2"
+//   }
+// ]
 
 function App() {
 
@@ -30,40 +29,43 @@ function App() {
 
   return (
     <>
-      <h2> Green Systems LED Inventory Check</h2>
+      <div Style="margin-left: 21px;
+                  margin-right: 21px;
+                  font-family: times new roman">
+        <h2> Green Systems LED Inventory Check</h2>
 
-      <Autocomplete
-        open={open}
-        onOpen={() => {
-          if (inputValue) {
-            setOpen(true);
-          }
-        }}
-        inputValue={inputValue}
-        onInputChange={(e, value) => {
-          setInputValue(value);
-          if (!value) {
-            setOpen(false);
-          }
-        }}
-        options={PartsList}
-        renderInput={(params) => (
-          <TextField {...params} label="Enter Part Number" variant="outlined" />
-        )}
-      />
+        <Autocomplete
+          open={open}
+          onOpen={() => {
+            if (inputValue) {
+              setOpen(true);
+            }
+          }}
+          inputValue={inputValue}
+          onInputChange={(e, value) => {
+            setInputValue(value);
+            if (!value) {
+              setOpen(false);
+            }
+          }}
+          options={PartsList}
+          renderInput={(params) => (
+            <TextField {...params} label="Enter Part Number" variant="outlined" />
+          )}
+        />
 
-      <div Style="border: 1px solid black;
+        <div Style="border: 1px solid black;
                   margin-top: 42px;
-                  margin-left: 15px;
-                  margin-right: 15px;
                   margin-bottom: 42px;">
-      </div>
+        </div>
 
+        {/* 
       {PartsMeta.map(data =>
         <>
-          <p Style="margin-left: 10px;"> {data.name} : {data.id} </p>
+          <p Style="margin-left: 10px;"> {data.name} : {data.id} -> {data.location} </p>
         </>
-      )}
+      )} */}
+      </div>
     </>
   );
 }
